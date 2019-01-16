94b71c98bb48f385a6cfd88f9c61cd8edf337ec8
Tanmoy Panigrahi <tanmoy.panigrahi2@gmail.com>
2019-01-16 18:15:33 -0500

    Updated Person2.java

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,9 +1,9 @@
 package edu.gatech.oad.antlab.person;
-
+import java.util.Random;
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
  * @author Bob
  * @version 1.1
@@ -11,37 +11,46 @@ package edu.gatech.oad.antlab.person;
 public class Person2 {
     /** Holds the persons real name */
     private String name;
-	 	/**
-	 * The constructor, takes in the persons
-	 * name
-	 * @param pname the person's real name
-	 */
-	 public Person2(String pname) {
-	   name = pname;
-	 }
-	/**
-	 * This method should take the string
-	 * input and return its characters in
-	 * random order.
-	 * given "gtg123b" it should return
-	 * something like "g3tb1g2".
-	 *
-	 * @param input the string to be modified
-	 * @return the modified string
-	 */
-	private String calc(String input) {
-	  System.out.println("Implementation");
-	  return "implementation";
-	}
-	/**
-	 * Return a string rep of this object
-	 * that varies with an input string
-	 *
-	 * @param input the varying string
-	 * @return the string representing the 
-	 *         object
-	 */
-	public String toString(String input) {
-	  return name + calc(input);
-	}
+    /**
+     * The constructor, takes in the persons
+     * name
+     * @param pname the person's real name
+     */
+    public Person2(String pname) {
+        name = pname;
+    }
+    /**
+     * This method should take the string
+     * input and return its characters in
+     * random order.
+     * given "gtg123b" it should return
+     * something like "g3tb1g2".
+     *
+     * @param input the string to be modified
+     * @return the modified string
+     */
+    private String calc(String input) {
+        Random random = new Random();
+        char arr[] = input.toCharArray();
+        for( int i=0 ; i<arr.length ; i++ )
+        {
+            int j = random.nextInt(arr.length);
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        return new String(arr);
+    }
+    /**
+     * Return a string rep of this object
+     * that varies with an input string
+     *
+     * @param input the varying string
+     * @return the string representing the
+     *         object
+     */
+    public String toString(String input) {
+        return name + calc(input);
+    }
 }
